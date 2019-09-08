3b02dd07a500aa9740fe4fde6299dd19408bef8f
Ryan Lee <drdgvhbh@gmail.com>
2019-09-07 23:02:35 -0400

    Use a readline library

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,3 @@
-use exitcode;
-
-use std::io::Write;
-
 extern crate pest;
 #[macro_use]
 extern crate pest_derive;
@@ -12,44 +8,55 @@ use crate::pest::Parser;
 #[grammar = "sqlite3.pest"]
 pub struct Sqlite3Parser;
 
-fn print_prompt() {
-    print!("db > ")
-}
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
 
 fn main() {
-    loop {
-        print_prompt();
-        if std::io::stdout().flush().is_err() {
-            std::process::exit(exitcode::IOERR)
-        }
-        let mut buffer = String::new();
-        if std::io::stdin().read_line(&mut buffer).is_err() {
-            std::process::exit(exitcode::IOERR)
-        }
-        buffer.truncate(buffer.trim_end().len());
-        let parse_result = Sqlite3Parser::parse(Rule::program, buffer.as_str());
-        if parse_result.is_err() {
-            println!("Unrecognized command \'{}\'.", buffer);
-            continue;
-        }
-        let ast = parse_result.unwrap();
-        for program in ast {
-            let statements = program.into_inner();
-            for statement in statements {
-                match statement.as_rule() {
-                    Rule::meta_command => {
-                        let meta_commands = statement.into_inner();
-                        for meta_command in meta_commands {
-                            match meta_command.as_rule() {
-                                Rule::exit => std::process::exit(exitcode::OK),
-                                _ => {}
+    let mut rl = Editor::<()>::new();
+    rl.load_history("history.txt");
+    'main: loop {
+        let readline = rl.readline("sqlite> ");
+        match readline {
+            Ok(buffer) => {
+                rl.add_history_entry(buffer.as_str());
+                let parse_result = Sqlite3Parser::parse(Rule::program, buffer.as_str());
+                if parse_result.is_err() {
+                    println!("Unrecognized command \'{}\'.", buffer);
+                    continue;
+                }
+                let ast = parse_result.unwrap();
+                for program in ast {
+                    let statements = program.into_inner();
+                    for statement in statements {
+                        match statement.as_rule() {
+                            Rule::meta_command => {
+                                let meta_commands = statement.into_inner();
+                                for meta_command in meta_commands {
+                                    match meta_command.as_rule() {
+                                        Rule::exit => {
+                                            break 'main;
+                                        }
+                                        _ => {}
+                                    }
+                                }
                             }
+                            Rule::EOI => {}
+                            _ => println!("Unsupported command \'{}\'.", buffer),
                         }
                     }
-                    Rule::EOI => {}
-                    _ => println!("Unsupported command \'{}\'.", buffer),
                 }
+            }
+            Err(ReadlineError::Interrupted) => {
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
             }
         }
     }
+    rl.save_history("history.txt").unwrap();
 }
