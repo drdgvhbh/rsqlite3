07abf7b533a880c67bb102c6f600361f4e26c3cc
Ryan Lee <drdgvhbh@gmail.com>
2019-09-07 23:52:18 -0400

    Make schema serializable and deserializable

--- src/schema.rs
+++ src/schema.rs
@@ -1,15 +1,17 @@
-#[derive(Debug)]
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
 enum Datatype {
     Integer = 1,
 }
 
-#[derive(Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 struct Column<'a> {
     name: &'a str,
     datatype: Datatype,
 }
 
-#[derive(Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 struct Table<'a> {
     name: &'a str,
     columns: Vec<Column<'a>>,
