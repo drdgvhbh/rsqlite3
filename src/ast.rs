7836c0c40df4f98e66f616ad15034b802f28d4d8
Ryan Lee <drdgvhbh@gmail.com>
2019-09-08 11:17:41 -0400

    Make AST use PartialEq trait

--- src/ast.rs
+++ src/ast.rs
@@ -1,36 +1,36 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Ast {
     Exit,
     Create(Table),
     Insert(Insertion),
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 pub enum Datatype {
     Integer = 1,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 pub struct Column {
     pub name: Option<String>,
     pub datatype: Datatype,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 pub struct Table {
     pub name: String,
     pub columns: Vec<Column>,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 pub struct Insertion {
     pub column_names: Option<Vec<String>>,
     pub values: Vec<Value>,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 pub enum Value {
     Integer(i64),
 }
