2a4abf1d83444e3b381d8d6c6d9f1fa490faa05b
Ryan <drdgvhbh@gmail.com>
2019-09-08 16:22:52 -0400

    Rename tables to table_definitions

--- src/executor.rs
+++ src/executor.rs
@@ -2,21 +2,22 @@ use crate::ast::Table;
 use std::collections::HashMap;
 
 pub struct Executor {
-    tables: HashMap<String, Table>,
+    table_definitions: HashMap<String, Table>,
 }
 
 pub fn new_executor() -> Executor {
     return Executor {
-        tables: HashMap::new(),
+        table_definitions: HashMap::new(),
     };
 }
 
 impl Executor {
     pub fn add_table(&mut self, table: &Table) -> Result<(), String> {
-        if self.tables.contains_key(&table.name) {
+        if self.table_definitions.contains_key(&table.name) {
             return Err(format!("table {} already exists", table.name).to_string());
         }
-        self.tables.insert(table.name.to_lowercase(), table.clone());
+        self.table_definitions
+            .insert(table.name.to_lowercase(), table.clone());
 
         Ok(())
     }
