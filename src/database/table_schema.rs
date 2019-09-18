fc22c931381d4325e1ab0e15a3997b52d84d3a67
Ryan Lee <drdgvhbh@gmail.com>
2019-09-18 13:21:10 -0400

    Add Datatype Enum

--- src/database/table_schema.rs
+++ src/database/table_schema.rs
@@ -1,4 +1,34 @@
-#[derive(Debug)]
+use serde::{Deserialize, Serialize};
+
+pub trait Serializer {
+    fn size(&self, obj: impl Serialize) -> usize;
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum DataType {
+    Boolean,
+    Char(usize),
+    Int,
+    Real,
+}
+
+impl DataType {
+    pub fn size(&self, serializer: impl Serializer) -> usize {
+        match self {
+            DataType::Boolean => serializer.size(false),
+            DataType::Char(length) => {
+                let mut dummy_string = String::new();
+                for _ in 0..*length {
+                    dummy_string.push('A');
+                }
+                serializer.size(dummy_string)
+            }
+            DataType::Int => serializer.size(std::i32::MAX),
+            DataType::Real => serializer.size(std::f32::MAX),
+        }
+    }
+}
+
 pub struct Schema {
     pub table_name: String,
 }
